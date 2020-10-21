f7caca03ecdef806901ad2cea795b55b477578bc
wboufade <wael.boufaden@advyteam.com>
2020-10-21 20:34:46 +0100

    - Removing static tags from Runner class and working with dynaming tags from cmd line

--- src/test/java/com/advyteam/weavin/runner/Runner.java
+++ src/test/java/com/advyteam/weavin/runner/Runner.java
@@ -8,7 +8,6 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src/test/resources/features/",
-        tags = "@WEAVIN-TIMELINE-0001",
         plugin = {"json:target/reports/cucumber.json"},
         glue = "com.advyteam.weavin")
 public class Runner {}
